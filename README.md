5e1b90f6106066a1fb31f7a9aa7cef4c2420eb3e
M3rcurylake <ankitmukherjee814@gmail.com>
2024-11-24 15:05:11 +0530

    add files and readme

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # TeleCam
-A Surveillance Camera Setup made with a ESP32-CAM and a PIR Sensor (HC-SR501) and a Telegram Bot
+
+This project integrates an ESP32-CAM module and a PIR motion sensor (HC-SR501) to develop a motion-activated camera system with Telegram bot integration for remote control and notifications. The system employs the **Universal Telegram Bot Library** and **ArduinoJson** for interfacing with the Telegram API.  
+
+
+### _Setup:_
+- **Setting Up Network Credentials** : Insert your network credentials in the following variables 
+  ```
+  const char* ssid = "*******"; 
+  const char* password = "*********";
+  ```
+
+- **Setting Up Telegram :** Create a new bot in telegram with [BotFather](t.me/botfather) with `/newbot` and follow the instructions to create your bot. Give it a name and username. If your bot is successfully created, you’ll receive a message with a link to access the bot and the bot token. Save the bot token because you’ll need it so that the ESP32 can interact with the bot.
+In your Telegram account, search for “IDBot” or open this link t.me/myidbot in your smartphone.
+Start a conversation with that bot and type `/getid`. You will get a reply back with your user ID, save that for later.
+now change the Bottoken and ChatID variables with your own Telegram bot token and ChatID
+
+    ```
+    String BOTtoken = "********:***************************";
+    String CHAT_ID = "*********";
+    ```
+
+
+- **Pinout and Interfacing:** Now follow the given pinout table to properly interface the ESP32-CAM with HC-SR501 using a FT232RL FTDI to TTL Serial Converter Module (remember that one jumper will be present in the FT232RL, blocking the 3.3V PIN and another at the PIR HC-SR501 at the 'H' position enabling multiple trigger mode, blocking the 'L' connection)
+
+    |ESP-32 CAM |FT232RL    |
+    |--------   |-------    |
+    |5V         |5V         |
+    |RX         |UOT        |
+    |TX         |UOR        |
+    |GND        |GND        |
+    
+    |ESP-32 CAM |PIR (HC-SR501) |
+    |-------    |-------        |
+    |3.3V       |VCC            |
+    |OUT        |GPIO13         |
+    |GND        |GND            |
+    
+    |ESP-32 CAM |ESP-32 CAM |
+    |-------    |-------        |
+    |GND       |GPIO0           |
+    
+
+- **Installing Requirements** : To interact with the Telegram bot, we’ll use the Universal Telegram Bot Library created by Brian Lough which provides an easy interface for the Telegram Bot API. The ArduinoJSON library will be installed automatically with the Universal Telegram Bot Library, if not, you will also have to install the ArduinoJson library. You can install both of them through library through the Arduino Library Manager.
+
+    
+- **Final Steps :** Flash the code with Arudino IDE, then remove the `GND GPIO0` Jumper Wire. Tune in to the 115200 baud band and press the `RST` button on the ESP32-CAM. After 20 seconds of warming up the surveillance setup should be up and running.
+To communicate with the ESP32-CAM, send a `/init` to the Telegram Bot we made earlier. It must output the following :
+   ```
+   /init: Displays a help menu to guide users.  
+   /start: Activates the camera server, enabling motion detection and image capture when movement is detected.  
+   /stop: Halts the camera server to conserve resources.  
+   ```
+
+**Hardware Configuration**:  
+- **PIR Sensor**: Configured with minimal sensitivity and delay in multiple trigger mode (H mode). Its output is connected to GPIO13 with a 45kΩ internal pulldown resistor with `pinMode(pin, mode);` where mode is `INPUT_PULLDOWN`  ([more info here](https://docs.espressif.com/projects/arduino-esp32/en/latest/api/gpio.html#pinmode)) to ensure stable low signals in idle states.  
+- **Status Indicators**:  
+  - GPIO33 controls a red LED to indicate active operation.  
+  - GPIO4 flashes the built-in flashlight, mimicking a camera torch, during image capture.  
+
+**Software Dependencies**:  
+The system leverages critical libraries such as `"wifi.h"` for connectivity and `"esp_camera.h"` for camera operations.  
+
+This project provides a simple and efficient solution for remote motion detection and image capture using a Telegram bot interface, offering practical applications in home security and automation.
+
+
+_Disclaimer : Made as a first semester ECE project, sorry for any errors caused within. Thanks._
+
---
+++ telecam_main.ino
@@ -0,0 +1,294 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include <UniversalTelegramBot.h>
+#include <ArduinoJson.h>
+
+#define TEAM = 'Neofetch'
+
+
+const char* ssid = "SRM";
+const char* password = "SRM@hostels";
+const int PIR_SENSOR_OUTPUT_PIN = 13;  /* PIR sensor O/P pin */
+int warm_up;
+
+
+String BOTtoken = "7547283694:AAGZax6_pZzN_KTyRcG6goZo1OcDeXuA1ug";  // Bot Token 
+
+String CHAT_ID = "7546122942";
+
+bool sendPhoto = false;
+
+WiFiClientSecure clientTCP;
+UniversalTelegramBot bot(BOTtoken, clientTCP);
+
+//Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+//CAMERA_MODEL_AI_THINKER Camera Init Code, taken from the "CamWebServer" Example
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+
+void configInitCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.grab_mode = CAMERA_GRAB_LATEST;
+
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+}
+
+void handleNewMessages(int numNewMessages) {
+  Serial.print("Handle New Messages: ");
+  Serial.println(numNewMessages);
+
+  for (int i = 0; i < numNewMessages; i++) {
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+    
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+    
+    String from_name = bot.messages[i].from_name;
+    if (text == "/init") {
+      String welcome = "Welcome , " + from_name + "\n";
+      welcome += "Use the following commands to interact with the ESP32-CAM \n";
+      welcome += "/init : Displays a help menu to guide users. \n";
+      welcome += "/start :Activates the camera server, enabling motion detection and image capture when movement is detected . \n";
+      welcome += "/stop : Halts the camera server to conserve resources. \n";
+      bot.sendMessage(CHAT_ID, welcome, "");
+    }
+    if (text == "/start") {
+      sendPhoto = true;
+      Serial.println("server loop started");
+    }
+    if (text == "/stop") {
+      sendPhoto = false;
+      Serial.println("server loop started");
+    }
+  }
+}
+
+String sendPhotoTelegram() {
+  const char* myDomain = "api.telegram.org";
+  String getAll = "";
+  String getBody = "";
+  digitalWrite(4, HIGH);  // Turn on the LED (flashlight)
+
+  //Dispose first picture because of bad quality
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  esp_camera_fb_return(fb); // dispose the buffered image
+  
+  // Take a new photo
+  fb = NULL;  
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+    return "Camera capture failed";
+  }  
+  digitalWrite(4, LOW);  // Turn off the LED (flashlight)
+  Serial.println("Connect to " + String(myDomain));
+
+
+  if (clientTCP.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+    
+    String head = "--Neofetch\r\nContent-Disposition: form-data; name=\"chat_id\"; \r\n\r\n" + CHAT_ID + "\r\n--Neofetch\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--Neofetch--\r\n";
+
+    size_t imageLen = fb->len;
+    size_t extraLen = head.length() + tail.length();
+    size_t totalLen = imageLen + extraLen;
+  
+    clientTCP.println("POST /bot"+BOTtoken+"/sendPhoto HTTP/1.1");
+    clientTCP.println("Host: " + String(myDomain));
+    clientTCP.println("Content-Length: " + String(totalLen));
+    clientTCP.println("Content-Type: multipart/form-data; boundary=Neofetch");
+    clientTCP.println();
+    clientTCP.print(head);
+  
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n=0;n<fbLen;n=n+1024) {
+      if (n+1024<fbLen) {
+        clientTCP.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen%1024>0) {
+        size_t remainder = fbLen%1024;
+        clientTCP.write(fbBuf, remainder);
+      }
+    }  
+    
+    clientTCP.print(tail);
+    
+    esp_camera_fb_return(fb);
+    
+    int waitTime = 10000;   // timeout 10 seconds
+    long startTimer = millis();
+    boolean state = false;
+    
+    while ((startTimer + waitTime) > millis()){
+      Serial.print(".");
+      delay(100);      
+      while (clientTCP.available()) {
+        char c = clientTCP.read();
+        if (state==true) getBody += String(c);        
+        if (c == '\n') {
+          if (getAll.length()==0) state=true; 
+          getAll = "";
+        } 
+        else if (c != '\r')
+          getAll += String(c);
+        startTimer = millis();
+      }
+      if (getBody.length()>0) break;
+    }
+    clientTCP.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody="Connection to api.telegram.org failed.";
+    Serial.println("Connection to api.telegram.org failed.");
+  }
+  return getBody;
+}
+
+void setup(){
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  //for esp32-cam red led and flashlight
+  pinMode(33, OUTPUT);
+  pinMode(4, OUTPUT);
+
+
+  //For PIR Sensor
+  pinMode(PIR_SENSOR_OUTPUT_PIN, INPUT_PULLDOWN);
+  Serial.println("Waiting For Power On Warm Up");
+  delay(20000); /* Power On Warm Up Delay */
+  Serial.println("PIR Ready!");
+
+
+  // Config and init the camera
+  configInitCamera();
+
+  // Connect to Wi-Fi
+  WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");                                                                                                                                                   
+  Serial.println(WiFi.localIP()); 
+}
+
+void loop() {
+  digitalWrite(33, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+  int sensor_output;
+  sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN);
+
+  if( sensor_output == HIGH  && sendPhoto ){
+    if( warm_up == 0 )
+     {
+      Serial.print("Warming Up\n\n");
+      warm_up = 1;
+      delay(2000);
+    }
+
+      Serial.print("Object detected\n\n");   
+      warm_up = 1;
+      Serial.println("Preparing photo");
+      sendPhotoTelegram(); 
+      delay(1000);
+  }
+    else{
+    if (!sendPhoto){
+      Serial.print("Cam Server Not Running\n\n");
+    }
+    else {
+      Serial.print("No object in sight\n\n");
+      warm_up = 0;
+    }
+    delay(1000);
+    } 
+
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    while (numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
